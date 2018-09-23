1d35385c62ee06d33228bb2b02b17d1c831eeb03
victornery <victornery.t@gmail.com>
2018-09-23 03:27:33 -0300

    Adicionado fatorial dentro dos métodos - Mac Nery

--- methods.php
+++ methods.php
@@ -10,29 +10,37 @@
     <?php
     $word = $_POST["word"];
     $number = $_POST["number"];
+    $fat = $_POST["fat"];
+
+    function calcFat($fat){
+        if ($fat <= 1){
+            $fat = $fat;
+        } else {
+            $fat *= calcFat($fat-1);
+        }
+        return $fat;
+    }
 
     /*$primos = array();
-    
     function numeroPrimo($parametro) {
         if ($parametro%2!=0 && $parametro%3!=0 && $parametro%5!=0 && $parametro%7!=0 && $parametro%11!=0){
             return 1;
         }
     }
-
-    function exibePrimosRecursivamente($numero) {
+    function exibePrimos($numero) {
         numeroPrimo($numero);
     }*/
 
 
-    echo 'Sua string é ' . $word .' <br>';
-    echo 'Sua string contem ' . strlen($word) . ' caracteres <br>';
-    echo 'String Reversa: ' . strrev(substr($word, 0, 254)) . '<br>';
+    echo 'Sua string é ' . $word .' <br><br>';
+    echo 'Sua string contem ' . strlen($word) . ' caracteres <br><br>';
+    echo 'String Reversa: ' . strrev(substr($word, 0, 254)) . '<br><br>';
     echo 'Sua string reversa contém caracteres ' . strlen(strrev(substr($word, 0, 254)));
-    echo '<br><br><br><br>';
-    echo 'Exibindo número primos de 0 até ' . $number ."<br>";
-
-    for($i = 1; $i <= $number; $i++){
+    echo '<br><br>';
+    echo "Seu fatorial (do número ". $fat .") é igual a: " . calcFat($fat) ."<br><br>";
+    echo 'Exibindo número primos de 0 até ' . $number ."<br><br>";
     
+    for($i = 1; $i <= $number; $i++){
                 $divisores = 0;
      
         for($j = $i; $j >= 1; $j--){
